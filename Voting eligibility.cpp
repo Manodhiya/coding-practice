0d09b1bc356ee56fdc2621f86edf7d51a044ece4
Manodhiya <manodhiyabharathi@gmail.com>
2026-03-04 19:14:42 +0530

    Add files via upload

---
+++ Voting eligibility.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    
+    cout<<"Enter your age:";
+    cin>>age;
+
+    if(age>=18)
+        cout<<"eligible for voting";
+    else
+        cout<<"not eligible for voting";
+    
+    return 0;
+}
